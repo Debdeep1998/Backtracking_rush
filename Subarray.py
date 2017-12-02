0dabdfea7413a08aa2c64b8704fa8ff9dfc57d24
Debdeep1998 <33013681+Debdeep1998@users.noreply.github.com>
2017-12-02 06:56:11 +0530

    Create Subarray.py
    Printing all possible subarrays in a given array.

---
+++ Subarray.py
@@ -0,0 +1,23 @@
+n=int(input())
+l=[]
+for i in range(n):
+    a=int(input())
+    l.append(a)
+k=[0]*n
+def subset(i,t):
+    if(i==n):
+        return
+    while(t<n):
+       k[i]=l[t]
+       
+       print(k[:i+1])  
+       subset(i+1,t+1)
+       t+=1
+       k[i]=0
+        
+subset(0,0)
+
+
+
+    
+  
